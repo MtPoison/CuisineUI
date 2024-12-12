7ee771ac636acbf20f8f61b30b0705f3aa107944
p0izon <115532860+MtPoison@users.noreply.github.com>
2024-12-12 19:19:31 +0100

    ajout du slider

--- Assets/Script/GameManger.cs
+++ Assets/Script/GameManger.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class GameManger : MonoBehaviour
 {
     [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject canvasHeat;
+    [SerializeField] private GameObject player;
     void Start()
     {
         canvas.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(player.transform.position, canvasHeat.transform.position);
+        if (distance < 5)
+        {
+            canvasHeat.SetActive(true);
+        }
+        else
+        {
+            canvasHeat.SetActive(false);
+        }
     }
+
 }
---
+++ Assets/Script/Heat.cs
@@ -0,0 +1,58 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderToParticles : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private ParticleSystem particleSystem; 
+     [SerializeField] private GameObject button;
+    [SerializeField] private TMP_Text count;
+    private ParticleSystem.EmissionModule emissionModule;
+    private ParticleSystem.MainModule mainModule;
+
+    void Start()
+    {
+        if (slider != null && particleSystem != null)
+        {
+            emissionModule = particleSystem.emission;
+            mainModule = particleSystem.main;
+
+            emissionModule.enabled = false;
+
+            slider.onValueChanged.AddListener(OnSliderValueChanged);
+
+            ApplySliderValue(slider.value);
+        }
+        else
+        {
+            Debug.LogWarning("Assurez-vous d'assigner le Slider et le ParticleSystem dans l'inspecteur.");
+        }
+    }
+
+    void OnSliderValueChanged(float value)
+    {
+        ApplySliderValue(value);
+    }
+
+    void ApplySliderValue(float value)
+    {
+        if (value > 0)
+        {
+            emissionModule.enabled = true;
+            float particleCount = Mathf.Lerp(5, 100, value);
+            emissionModule.rateOverTime = particleCount;
+            mainModule.startSpeed = Mathf.Lerp(1f, 10f, value);
+            mainModule.startSize = Mathf.Lerp(0.1f, 2f, value);
+            count.text = $"{(int) Mathf.Lerp(0f, 100f, value)}";
+            Vector3 currentRotation = button.transform.rotation.eulerAngles;
+            button.transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y, Mathf.Lerp(-90f, 90f, value));
+        }
+        else
+        {
+            emissionModule.enabled = false;
+        }
+
+        Debug.Log("Valeur Slider : " + value + " | Particules actives : " + emissionModule.enabled);
+    }
+}
